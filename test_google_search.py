df2dc2382b5af7dbd4e3b87213f556d44c722dea
Goncharov Anton <a.goncharrov@icloud.com>
2024-12-09 16:06:51 +0300

    upd autoformat

--- test_google_search.py
+++ test_google_search.py
@@ -1,15 +1,18 @@
 from selene.api import s
 from selene import browser, have, be
 
+
 def test_google_open():
     browser.open("https://google.com/ncr")
     assert browser.driver.current_url.startswith("https://www.google.com/")
+
 
 def test_google_search():
     browser.open("https://google.com/ncr")
     s('[name=q]').should(be.blank).type('pytest github')
     s('[name=btnK]').click()
     s('.MBeuO').should(have.text('The pytest framework makes it easy to write small tests, yet ...'))
+
 
 def test_negative_google_search():
     browser.open("https://google.com/ncr")
